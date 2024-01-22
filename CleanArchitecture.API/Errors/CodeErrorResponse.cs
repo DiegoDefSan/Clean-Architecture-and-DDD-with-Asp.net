5bf8f122e5f17e7ce7c6ffd9e5e38573d486822f
DiegoDefSan <defsan35@gmail.com>
2024-01-22 12:11:44 -0500

    feat: middleware

---
+++ CleanArchitecture.API/Errors/CodeErrorResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace CleanArchitecture.API.Errors
+{
+    public class CodeErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public CodeErrorResponse(int statusCode, string? message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageStatusCode(statusCode); // Si el mensaje es null
+        }
+
+        private string GetDefaultMessageStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "El request enviado tiene errores",
+                401 => "No tienes autorización para este recurso",
+                404 => "No se encontró el recurso solicitado",
+                500 => "Se producieron errores en el servidor",
+                _ => string.Empty
+            };
+        }
+    }
+}
--- CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
+++ CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45b63249a2dc3619525f8e4e9b123db941bcf84b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c7d454e02353985cacebd7c7f8aea39185c2a43")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
